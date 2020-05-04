89baf21bd31277f9fe48fd0dc75eb0242bf19574
Mikhail Baranov <baranov@arrival.com>
2020-05-04 18:55:24 +0300

    More detailed requirements. Now pipenv doesn't fails on Mac

--- tests/integration.py
+++ tests/integration.py
@@ -1,16 +1,33 @@
 import datetime
+import logging
+import sys
+from unittest.mock import MagicMock
 
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+if sys.platform != 'linux':
+    bluepy_mock = MagicMock()
+    p_mock = MagicMock()
+    bluepy_mock.btle.Peripheral.return_value = p_mock
+
+    sys.modules['bluepy'] = bluepy_mock
 from lywsd02 import Lywsd02Client
 
 mac = '3F:59:C8:80:70:BE'
 
-client = Lywsd02Client(mac)
+
+def create_client():
+    return Lywsd02Client(mac)
+
 
 if __name__ == '__main__':
+    client = create_client()
     dt = datetime.datetime.now()
     with client.connect():
         temperature = client.temperature
         humidity = client.humidity
-        time = client.time[0]
-        battery = client.battery
-    print(battery)
+        time = client.time
+
+    print(temperature, humidity, time)
